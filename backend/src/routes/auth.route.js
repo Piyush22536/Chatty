ac4de51eda6962b422acef6a2f82b29a391aa5eb
Piyush Kumar <154805249+Piyush22536@users.noreply.github.com>
2026-03-09 00:15:34 +0530

    update rate limiter middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+import { tokenBucketLimiter } from "../middleware/tokenBucket.middleware.js";
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", tokenBucketLimiter, signup);
+router.post("/login", tokenBucketLimiter, login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
