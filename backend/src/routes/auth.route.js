f9dd5412ba1dea81a17fb10959df5c7363fe6c6a
Piyush Kumar <154805249+Piyush22536@users.noreply.github.com>
2026-03-09 00:52:04 +0530

    implement async message processing using BullMQ and Redis

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,11 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-<<<<<<< HEAD
-
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-=======
 import { tokenBucketLimiter } from "../middleware/tokenBucket.middleware.js";
 const router = express.Router();
 
 router.post("/signup", tokenBucketLimiter, signup);
 router.post("/login", tokenBucketLimiter, login);
->>>>>>> main
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
