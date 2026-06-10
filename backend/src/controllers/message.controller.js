d8b69324fbc598c1caa7b755b8fe433042c1a496
Piyush Kumar <154805249+Piyush22536@users.noreply.github.com>
2026-06-10 15:57:53 +0530

    bug fixed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -22,7 +22,6 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
---------
 export const getMessages = async (req, res) => {
   try {
     const { id: userToChatId } = req.params;
